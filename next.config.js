faf6603d82186f04caeaa38666b7ff26ea1dc4eb
luigi009 <luigifanaro@hotmail.com>
2021-02-22 17:13:46 -0300

    update

--- next.config.js
+++ next.config.js
@@ -2,3 +2,15 @@ module.exports = {
     basePath: process.env.NEXT_PUBLIC_BASE_PATH,
     assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
 }
+
+module.exports = {
+    exportPathMap: async function (
+      defaultPathMap,
+      { dev, dir, outDir, distDir, buildId }
+    ) {
+      return {
+        '/': { page: '/' },
+        '/about': { page: '/about' },
+      }
+    },
+  }
